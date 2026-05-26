f87e0357c430af04239a1311621cfc20b7d3d357
Foxx05 <theocolpaert8@gmail.com>
2026-05-26 23:08:04 +0200

    indent multilineChartt.tsx

--- tfe/app/components/multilineChart.tsx
+++ tfe/app/components/multilineChart.tsx
@@ -37,54 +37,54 @@ export default function MultiLineChart({
   data,
 }: Props) {
 
-const chartData = {
-  labels: data.map((item) => item.label),
+  const chartData = {
+    labels: data.map((item) => item.label),
 
-  datasets: [
-    {
-      label: "Flowers",
-      data: data.map((item) => item.flowers),
-      borderColor: "#d6d6d6",
-      backgroundColor: "#d6d6d6",
-      tension: 0.3,
-    },
-    {
-      label: "Green fruits",
-      data: data.map((item) => item.green),
-      borderColor: "#4caf50",
-      backgroundColor: "#4caf50",
-      tension: 0.3,
-    },
-    {
-      label: "Yellow fruits",
-      data: data.map((item) => item.yellow),
-      borderColor: "#fbd704",
-      backgroundColor: "#fbd704",
-      tension: 0.3,
-    },
-    {
-      label: "Red fruits",
-      data: data.map((item) => item.red),
-      borderColor: "#d44",
-      backgroundColor: "#d44",
-      tension: 0.3,
-    },
-  ],
-};
+    datasets: [
+      {
+        label: "Flowers",
+        data: data.map((item) => item.flowers),
+        borderColor: "#d6d6d6",
+        backgroundColor: "#d6d6d6",
+        tension: 0.3,
+      },
+      {
+        label: "Green fruits",
+        data: data.map((item) => item.green),
+        borderColor: "#4caf50",
+        backgroundColor: "#4caf50",
+        tension: 0.3,
+      },
+      {
+        label: "Yellow fruits",
+        data: data.map((item) => item.yellow),
+        borderColor: "#fbd704",
+        backgroundColor: "#fbd704",
+        tension: 0.3,
+      },
+      {
+        label: "Red fruits",
+        data: data.map((item) => item.red),
+        borderColor: "#d44",
+        backgroundColor: "#d44",
+        tension: 0.3,
+      },
+    ],
+  };
 
- return (
-  <>
-    <h1>{title}</h1>
+  return (
+    <>
+      <h1>{title}</h1>
 
-    <div style={{ height: "300px" }}>
-      <Line
-        data={chartData}
-        options={{
-          responsive: true,
-          maintainAspectRatio: false,
-        }}
-      />
-    </div>
-  </>
-);
+      <div style={{ height: "300px" }}>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+          }}
+        />
+      </div>
+    </>
+  );
 }
